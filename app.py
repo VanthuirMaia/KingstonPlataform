2c272510bdc3f575c650ae2ed2488efe5602f82b
Bowie <vanmaiasf@gmail.com>
2025-02-04 19:51:30 -0300

    Criado templates Base, Index, Contato e Projetos

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# Rota de Projetos
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+# Rota de Contato
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
-
